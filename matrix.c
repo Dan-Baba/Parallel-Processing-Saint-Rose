f19794051dc9c1a2e30f85bcd3fe4aec96f64bda
Dan-Baba <danfoote104227@gmail.com>
2019-02-25 12:48:46 -0500

    Finished Matrix Multiply

--- matrix.c
+++ matrix.c
@@ -1,22 +1,50 @@
 #include <stdio.h>
+#include<pthread.h>
+#define NUM_ROWS 2
+#define NUM_COLS 2
+#define NUM_CELLS NUM_ROWS*NUM_COLS
+
+struct row_column {
+    int row;
+    int column;
+};
+int arrA[2][2] = {
+    {1, 2},
+    {3, 4}
+};
+int arrB[2][2] = {
+    {5, 6},
+    {7, 8}
+};
+int arrC[2][2];
 
 void printMatrix(int array[2][2]);
+void *DotProduct(void *arguments);
 int main() {
-    int arrA[2][2] = {
-        {1, 2},
-        {3, 4}
-    };
-    int arrB[2][2] = {
-        {5, 6},
-        {7, 8}
-    };
+    pthread_t threads[NUM_CELLS];
+    struct row_column args[NUM_CELLS];
+    // Get args in structs ready to pass to function.
+    for (int i = 0; i < NUM_ROWS; i++) {
+        for (int j = 0; j < NUM_COLS; j++) {
+            int currIndex = (i*NUM_COLS) + j;
+            args[currIndex].row = i;
+            args[currIndex].column = j;
+        }
+    }
 
-    printMatrix(arrA);
-    return 0;
-}
+    // Make threads, passing args
+    for (int i = 0; i < NUM_CELLS; i++) {
+        pthread_create(&threads[i], NULL, DotProduct, (void *)&args[i]);
+    }
 
-void matrixMultiply(int matrixA[2][2], int matrixB[2][2]) {
+    // Wait for threads to finish
+    for (int i = 0; i < NUM_CELLS; i++) {
+        pthread_join(threads[i], NULL);
+    }
 
+    // Print result
+    printMatrix(arrC);
+    pthread_exit(NULL);
 }
 
 void printMatrix(int array[2][2]) {
@@ -26,4 +54,17 @@ void printMatrix(int array[2][2]) {
         }
         printf("\n");
     }
+}
+
+void *DotProduct(void *arguments) {
+  struct row_column *args = (struct row_column *) arguments;
+  int row = args->row;
+  int col = args->column;
+  printf("Thread calculating dot product for {%d, %d}\n", row, col);
+  int sum = 0;
+  for (int i = 0; i < NUM_COLS; i++) {
+      sum += arrA[row][i] * arrB[i][col];
+  }
+  arrC[row][col] = sum;
+  pthread_exit(NULL);
 }
