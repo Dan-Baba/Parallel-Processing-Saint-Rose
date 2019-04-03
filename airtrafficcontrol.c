5b98201c33d757342c0518225bbd9d379fc19383
Dan-Baba <danfoote104227@gmail.com>
2019-04-03 10:01:02 -0400

    Finished air traffic control assignment.

--- airtrafficcontrol.c
+++ airtrafficcontrol.c
@@ -6,12 +6,15 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-#define NUM_THREADS 20
+#define NUM_THREADS 50
 #define CONTROL_TIMES 100
 #define CONTROL_SLEEP_TIME 10
 
 int clearances_remaining = 5;
+int num_planes = NUM_THREADS - 1;
+
 pthread_mutex_t clearance_mutex;
+pthread_mutex_t num_planes_mutex;
 pthread_cond_t clearance_cv;
 
 void *Plane(void *threadid) {
@@ -27,51 +30,55 @@ void *Plane(void *threadid) {
 
     for (int i = 0; i < leg; i++) {
         pthread_mutex_lock(&clearance_mutex);
-
         while (true) {
             if (clearances_remaining > 0) {
                 clearances_remaining--;
-                // TODO: figure out flight-time
-                printf("Thread #%ld, leg #%d, flight-time\n", tid, (i + 1));
                 break;
             } else {
-                //printf("Thread #%ld waiting for broadcast\n", tid, (i + 1));
+                printf("Thread #%ld waiting for control to broadcast.\n", tid);
                 pthread_cond_wait(&clearance_cv, &clearance_mutex);
-                //printf("Thread #%ld waking up\n", tid, (i + 1));
             }
         }
-        //printf("Thread #%ld releasing lock\n", tid, (i + 1));
         pthread_mutex_unlock(&clearance_mutex);
-        // Generate random number between 1 - 3 and sleep.
-        int sleep_time = (rand_r(&seed) % 3) + 1;
-        sleep(sleep_time);
+
+        // Generate random number between 1 - 3, this is the flight-time.
+        int flight_time = (rand_r(&seed) % 3) + 1;
+        printf("Thread #%ld, leg #%d, flight-time %d\n", tid, (i + 1), flight_time);
+        sleep(flight_time);
+        pthread_mutex_lock(&clearance_mutex);
+        clearances_remaining++;
+        pthread_mutex_unlock(&clearance_mutex);
     }
 
+    pthread_mutex_lock(&num_planes_mutex);
+    num_planes--;
+    pthread_mutex_unlock(&num_planes_mutex);
     printf("Thread #%ld finished!\n", tid);
     pthread_exit(NULL);
 }
 
+// Control actually keeps looping until all airplanes are done.
 void *Control(void *threadid) {
-    for (int i = 0; i < CONTROL_TIMES; i++) {
+    pthread_mutex_lock(&num_planes_mutex);
+    while (num_planes > 0) {
+        pthread_mutex_unlock(&num_planes_mutex);
         pthread_mutex_lock(&clearance_mutex);
-        if (clearances_remaining <= 0) {
-            clearances_remaining += 5;
+        if (clearances_remaining > 0) {
             pthread_cond_broadcast(&clearance_cv);
-            // TODO: Is this what's meant by print signaled condition variable?
-            printf("Control making clearance\n");
+            printf("Control announcing %d clearances\n", clearances_remaining);
         }
         pthread_mutex_unlock(&clearance_mutex);
-        //printf("Control sleeping\n");
         sleep(CONTROL_SLEEP_TIME);
-        //printf("Control waking up\n");
+        pthread_mutex_lock(&num_planes_mutex);
     }
-
+    pthread_mutex_unlock(&num_planes_mutex);
     printf("Control exiting...\n");
     pthread_exit(NULL);
 }
 
 int main (int argc, char *argv[]) {
     pthread_mutex_init(&clearance_mutex, NULL);
+    pthread_mutex_init(&num_planes_mutex, NULL);
     pthread_cond_init(&clearance_cv, NULL);
 
     pthread_t threads[NUM_THREADS];
